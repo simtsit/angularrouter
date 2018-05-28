3b0fb40f964fb752453df5ae3cc41d0185783196
Symeon Tsitoglou <simtsit@gmail.com>
2018-05-28 23:06:51 +0200

    Created the basic template. Functionality pendind.

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Angular Router Example!';
+  subheader = 'Click one of the links at the sidebar.';
+  content = {
+    body: "Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body "
+  };
+
+
+}
