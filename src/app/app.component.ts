fc53be95b78f75a9536de06b297d75ec33518157
Symeon Tsitoglou <simtsit@gmail.com>
2018-05-29 22:27:28 +0200

    Option URLs are now working. Pending change text functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular Router Example!';
   subheader = 'Click one of the links at the sidebar.';
+  
   content = {
     body: "Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body Body "
   };
 
 
+  options = [{
+    "name" : "name1",
+    "body" : "111 111 111 111 111 111",
+    "url" : "case-1"
+  },{
+    "name" : "name2",
+    "body" : "222 222 222 222 222 222",
+    "url" : "case-2"
+  },{
+    "name" : "name3",
+    "body" : "333 333 333 333 333 333",
+    "url" : "case-3"
+  },{
+    "name" : "name4",
+    "body" : "444 444 444 444 444 444",
+    "url" : "case-4"
+  },{
+    "name" : "name5",
+    "body" : "555 555 555 555 555 555",
+    "url" : "case-5"
+  }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { BodyholderComponent } from './bodyholder/bodyholder.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyholderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+   {
+      path: 'case-1',
+      component: BodyholderComponent
+   },{
+      path: 'case-2',
+      component: BodyholderComponent
+   },{
+      path: 'case-3',
+      component: BodyholderComponent
+   },{
+      path: 'case-4',
+      component: BodyholderComponent
+   },{
+      path: 'case-5',
+      component: BodyholderComponent
+   }
+])
   ],
   providers: [],
   bootstrap: [AppComponent]
